e52dbd5d8f9d19b62ffde701d61dcaa7a5e01ced
Kevin <quebin31@gmail.com>
2022-07-11 22:17:37 -0500

    feat: Add unit tests for the remaining operations

--- preferenceshelper/src/test/kotlin/com/example/datastorehelper/PreferencesHelperTest.kt
+++ preferenceshelper/src/test/kotlin/com/example/datastorehelper/PreferencesHelperTest.kt
@@ -1,9 +1,7 @@
 package com.example.datastorehelper
 
 import android.content.Context
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.test.core.app.ApplicationProvider
 import com.example.datastorehelper.test.CoroutinesTestRule
@@ -22,8 +20,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import strikt.api.expectThat
-import strikt.assertions.containsExactly
-import strikt.assertions.isEqualTo
+import strikt.assertions.*
 
 private val Context.testDataStore by preferencesDataStore(name = "testDataStore")
 
@@ -99,5 +96,59 @@ class PreferencesHelperTest {
         job.cancelAndJoin()
 
         expectThat(collectedValues).containsExactly(expectedValues)
+    }
+
+    @Test
+    fun `test deleting a value`() = runTest {
+        val booleanKey = booleanPreferencesKey(name = "keyA")
+
+        preferencesHelper.save(booleanKey, true)
+
+        expectThat(preferencesHelper.delete(booleanKey))
+            .isNotNull()
+            .isTrue()
+
+        expectThat(preferencesHelper.get(booleanKey))
+            .isNull()
+
+        expectThat(preferencesHelper.delete(booleanKey))
+            .isNull()
+    }
+
+    @Test
+    fun `test batch operations`() = runTest {
+        val floatKey = floatPreferencesKey(name = "keyA")
+        val doubleKey = doublePreferencesKey(name = "keyB")
+        val setKey = stringSetPreferencesKey(name = "keyC")
+
+        preferencesHelper.save(doubleKey, 2.0)
+        var deletedDouble: Double? = null
+        var updatedSet: Set<String> = emptySet()
+
+        preferencesHelper.batch {
+            save(floatKey, 3f)
+            deletedDouble = delete(doubleKey)
+            updatedSet = update(setKey, default = emptySet()) { set ->
+                set.plus("string")
+            }
+        }
+
+        expectThat(preferencesHelper.get(floatKey)).isEqualTo(3f)
+        expectThat(preferencesHelper.get(doubleKey)).isNull()
+        expectThat(deletedDouble).isEqualTo(2.0)
+        expectThat(preferencesHelper.get(setKey)).isEqualTo(updatedSet)
+    }
+
+    @Test
+    fun `test clearing preferences`() = runTest {
+        val keyA = intPreferencesKey(name = "keyA")
+        val keyB = intPreferencesKey(name = "keyB")
+        val keyC = intPreferencesKey(name = "keyC")
+
+        preferencesHelper.clear()
+
+        expectThat(preferencesHelper.get(keyA)).isNull()
+        expectThat(preferencesHelper.get(keyB)).isNull()
+        expectThat(preferencesHelper.get(keyC)).isNull()
     }
 }
