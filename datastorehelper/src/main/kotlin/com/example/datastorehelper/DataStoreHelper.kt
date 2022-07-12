7f0870e83ed82a77e472ca7cb24d3e68cdd5a32d
Kevin <quebin31@gmail.com>
2022-07-11 22:17:31 -0500

    chores: Rename key class

--- datastorehelper/src/main/kotlin/com/example/datastorehelper/DataStoreHelper.kt
+++ datastorehelper/src/main/kotlin/com/example/datastorehelper/DataStoreHelper.kt
@@ -1,33 +1,25 @@
 package com.example.datastorehelper
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import kotlinx.coroutines.flow.Flow
-
-/**
- * Create a new [DataStoreHelper] that wraps a [DataStore]<[Preferences]>.
- */
-fun DataStoreHelper(dataStore: DataStore<Preferences>): DataStoreHelper =
-    DataStoreHelperImpl(dataStore)
 
 interface BatchScope {
 
     /**
      * Save the given [key]-[value] pair, similar to [DataStoreHelper.save].
      */
-    fun <T> save(key: DataStoreKey<T>, value: T)
+    fun <T : Any> save(key: DataStoreHelperKey<T>, value: T)
 
     /**
      * Update the value linked to [key], if no such value exists then [default] is used
      * and passed to [transform], similar to [DataStoreHelper.update].
      */
-    fun <T> update(key: DataStoreKey<T>, default: T, transform: (T) -> T): T
+    fun <T : Any> update(key: DataStoreHelperKey<T>, default: T, transform: (T) -> T): T
 
     /**
      * Delete the value linked to [key] and returns the deleted value, returns `null` if no
      * value was deleted.
      */
-    fun <T> delete(key: DataStoreKey<T>): T?
+    fun <T : Any> delete(key: DataStoreHelperKey<T>): T?
 }
 
 interface DataStoreHelper {
@@ -36,30 +28,30 @@ interface DataStoreHelper {
      * Get a [Flow] which emits values linked to [key], may emit `null` if no such key-value
      * pair exists.
      */
-    fun <T> getAsFlow(key: DataStoreKey<T>): Flow<T?>
+    fun <T : Any> getAsFlow(key: DataStoreHelperKey<T>): Flow<T?>
 
     /**
      * Get a [T] value from the inner data store, if no such key-value pair exists this returns
      * `null`.
      */
-    suspend fun <T> get(key: DataStoreKey<T>): T?
+    suspend fun <T : Any> get(key: DataStoreHelperKey<T>): T?
 
     /**
      * Save the given [key]-[value] pair in the data store.
      */
-    suspend fun <T> save(key: DataStoreKey<T>, value: T)
+    suspend fun <T : Any> save(key: DataStoreHelperKey<T>, value: T)
 
     /**
      * Update the value linked to [key], if no such key-value pair exists, then [default] is
      * used, [transform] should use the current value and return the new one.
      */
-    suspend fun <T> update(key: DataStoreKey<T>, default: T, transform: (T) -> T): T
+    suspend fun <T : Any> update(key: DataStoreHelperKey<T>, default: T, transform: (T) -> T): T
 
     /**
      * Delete the [key]-value pair from data store and return the deleted value, returns `null` if
      * no value was deleted.
      */
-    suspend fun <T> delete(key: DataStoreKey<T>): T?
+    suspend fun <T : Any> delete(key: DataStoreHelperKey<T>): T?
 
     /**
      * Perform a batch of operations atomically, useful to avoid multiple separate write
--- datastorehelper/src/main/kotlin/com/example/datastorehelper/DataStoreHelperImpl.kt
+++ datastorehelper/src/main/kotlin/com/example/datastorehelper/DataStoreHelperImpl.kt
@@ -12,21 +12,21 @@ import kotlinx.coroutines.flow.map
 internal class DataStoreHelperImpl(private val dataStore: DataStore<Preferences>) :
     DataStoreHelper {
 
-    override fun <T> getAsFlow(key: DataStoreKey<T>): Flow<T?> =
+    override fun <T : Any> getAsFlow(key: DataStoreHelperKey<T>): Flow<T?> =
         dataStore.data.map { snapshot ->
             snapshot[key.toPreferencesKey() as Preferences.Key<T>]
         }
 
-    override suspend fun <T> get(key: DataStoreKey<T>): T? =
+    override suspend fun <T : Any> get(key: DataStoreHelperKey<T>): T? =
         dataStore.data.firstOrNull()?.get(key.toPreferencesKey() as Preferences.Key<T>)
 
-    override suspend fun <T> save(key: DataStoreKey<T>, value: T) {
+    override suspend fun <T : Any> save(key: DataStoreHelperKey<T>, value: T) {
         dataStore.edit { preferences ->
             preferences[key.toPreferencesKey() as Preferences.Key<T>] = value
         }
     }
 
-    override suspend fun <T> update(key: DataStoreKey<T>, default: T, transform: (T) -> T): T {
+    override suspend fun <T : Any> update(key: DataStoreHelperKey<T>, default: T, transform: (T) -> T): T {
         val preferencesKey = key.toPreferencesKey() as Preferences.Key<T>
         val snapshot = dataStore.edit { preferences ->
             preferences[preferencesKey] = transform(preferences[preferencesKey] ?: default)
@@ -35,7 +35,7 @@ internal class DataStoreHelperImpl(private val dataStore: DataStore<Preferences>
         return snapshot[preferencesKey]!!
     }
 
-    override suspend fun <T> delete(key: DataStoreKey<T>): T? = get(key)?.also {
+    override suspend fun <T : Any> delete(key: DataStoreHelperKey<T>): T? = get(key)?.also {
         dataStore.edit { preferences ->
             preferences.minusAssign(key.toPreferencesKey() as Preferences.Key<T>)
         }
@@ -56,18 +56,18 @@ internal class DataStoreHelperImpl(private val dataStore: DataStore<Preferences>
 
 @Suppress("UNCHECKED_CAST")
 private class BatchScopeImpl(private val mutablePreferences: MutablePreferences) : BatchScope {
-    override fun <T> save(key: DataStoreKey<T>, value: T) {
+    override fun <T : Any> save(key: DataStoreHelperKey<T>, value: T) {
         mutablePreferences[key.toPreferencesKey() as Preferences.Key<T>] = value
     }
 
-    override fun <T> update(key: DataStoreKey<T>, default: T, transform: (T) -> T): T {
+    override fun <T : Any> update(key: DataStoreHelperKey<T>, default: T, transform: (T) -> T): T {
         val preferencesKey = key.toPreferencesKey() as Preferences.Key<T>
         return transform(mutablePreferences[preferencesKey] ?: default).also { newValue ->
             mutablePreferences[preferencesKey] = newValue
         }
     }
 
-    override fun <T> delete(key: DataStoreKey<T>): T? {
+    override fun <T : Any> delete(key: DataStoreHelperKey<T>): T? {
         val preferencesKey = key.toPreferencesKey() as Preferences.Key<T>
         return mutablePreferences[preferencesKey]?.also {
             mutablePreferences.minusAssign(preferencesKey)
